nette/tracy

IBarPanel, ILogger: added typehints to interfaces (BC break)

src/Bridges/Psr/PsrToTracyLoggerAdapter.php
@@ -38,7 +38,7 @@ class PsrToTracyLoggerAdapter implements Tracy\ILogger
 	}
 
 
-	public function log($value, $priority = self::INFO)
+	public function log($value, string $priority = self::INFO)
 	{
 		if ($value instanceof \Throwable) {
 			$message = $value->getMessage();

src/Tracy/FireLogger.php
@@ -32,7 +32,7 @@ class FireLogger implements ILogger
 	 * Sends message to FireLogger console.
 	 * @param  mixed  $message
 	 */
-	public function log($message, $priority = self::DEBUG): bool
+	public function log($message, string $priority = self::DEBUG): bool
 	{
 		if (!isset($_SERVER['HTTP_X_FIRELOGGER']) || headers_sent()) {
 			return false;

src/Tracy/ILogger.php
@@ -23,5 +23,5 @@ interface ILogger
 		EXCEPTION = 'exception',
 		CRITICAL = 'critical';
 
-	function log($value, $priority = self::INFO);
+	function log($value, string $priority = self::INFO);
 }

src/Tracy/Logger.php
@@ -52,7 +52,7 @@ class Logger implements ILogger
 	 * @param  string  $priority  one of constant ILogger::INFO, WARNING, ERROR (sends email), EXCEPTION (sends email), CRITICAL (sends email)
 	 * @return string|null logged error filename
 	 */
-	public function log($message, $priority = self::INFO)
+	public function log($message, string $priority = self::INFO): ?string
 	{
 		if (!$this->directory) {
 			throw new \LogicException('Logging directory is not specified.');

moderntv/nette-tracy

src/Bridges/Psr/PsrToTracyLoggerAdapter.php
@@ -38,7 +38,7 @@ class PsrToTracyLoggerAdapter implements Tracy\ILogger
 	}
 
 
-	public function log($value, $level = self::INFO)
+	public function log($value, string $level = self::INFO)
 	{
 		if ($value instanceof \Throwable) {
 			$message = Tracy\Helpers::getClass($value) . ': ' . $value->getMessage() . ($value->getCode() ? ' #' . $value->getCode() : '') . ' in ' . $value->getFile() . ':' . $value->getLine();

src/Tracy/Logger/FireLogger.php
@@ -32,7 +32,7 @@ class FireLogger implements ILogger
 	 * Sends message to FireLogger console.
 	 * @param  mixed  $message
 	 */
-	public function log($message, $level = self::DEBUG): bool
+	public function log($message, string $level = self::DEBUG): bool
 	{
 		if (!isset($_SERVER['HTTP_X_FIRELOGGER']) || headers_sent()) {
 			return false;

src/Tracy/Logger/ILogger.php
@@ -23,5 +23,5 @@ interface ILogger
 		EXCEPTION = 'exception',
 		CRITICAL = 'critical';
 
-	function log($value, $level = self::INFO);
+	function log($value, string $level = self::INFO);
 }

src/Tracy/Logger/Logger.php
@@ -52,7 +52,7 @@ class Logger implements ILogger
 	 * @param  string  $level  one of constant ILogger::INFO, WARNING, ERROR (sends email), EXCEPTION (sends email), CRITICAL (sends email)
 	 * @return string|null logged error filename
 	 */
-	public function log($message, $level = self::INFO)
+	public function log($message, string $level = self::INFO): ?string
 	{
 		if (!$this->directory) {
 			throw new \LogicException('Logging directory is not specified.');